e2e1a994267498cd2ab5ddb574aec28c38b3f19b
Punit Arani <punitsai36@gmail.com>
2024-07-15 07:10:31 -0700

    Update LevelOneCard.tsx to only display update if data set is complete

--- components/LevelOneCard.tsx
+++ components/LevelOneCard.tsx
@@ -13,17 +13,65 @@ interface LevelOneCardProps {
 }
 
 export default function LevelOneCard({ levelOneContent }: LevelOneCardProps) {
-  const [lastLevelOne, setLastLevelOne] = useState<LevelOneEquityContent | undefined>(undefined)
+  const [askData, setAskData] = useState<{ price: number | undefined; size: number | undefined }>({
+    price: undefined,
+    size: undefined,
+  })
+  const [markData, setMarkData] = useState<{
+    mark: number | undefined
+    change: number | undefined
+    percentChange: number | undefined
+  }>({ mark: undefined, change: undefined, percentChange: undefined })
+  const [bidData, setBidData] = useState<{ price: number | undefined; size: number | undefined }>({
+    price: undefined,
+    size: undefined,
+  })
 
   useEffect(() => {
     if (levelOneContent) {
-      setLastLevelOne(levelOneContent as LevelOneEquityContent)
+      if (
+        (levelOneContent.ASK_PRICE &&
+          levelOneContent.ASK_PRICE > 0 &&
+          levelOneContent.ASK_PRICE !== askData.price) &&
+        (levelOneContent.ASK_SIZE &&
+          levelOneContent.ASK_SIZE > 0 &&
+          levelOneContent.ASK_SIZE !== askData.size)
+      ) {
+        setAskData({ price: levelOneContent.ASK_PRICE, size: levelOneContent.ASK_SIZE })
+      }
+      if (
+        (levelOneContent.MARK &&
+          levelOneContent.MARK > 0 &&
+          levelOneContent.MARK !== markData.mark) &&
+        (levelOneContent.MARK_CHANGE &&
+          levelOneContent.MARK_CHANGE > 0 &&
+          levelOneContent.MARK_CHANGE !== markData.change) &&
+        (levelOneContent.MARK_CHANGE_PERCENT &&
+          levelOneContent.MARK_CHANGE_PERCENT > 0 &&
+          levelOneContent.MARK_CHANGE_PERCENT !== markData.percentChange)
+      ) {
+        setMarkData({
+          mark: levelOneContent.MARK,
+          change: levelOneContent.MARK_CHANGE,
+          percentChange: levelOneContent.MARK_CHANGE_PERCENT,
+        })
+      }
+      if (
+        (levelOneContent.BID_PRICE &&
+          levelOneContent.BID_PRICE > 0 &&
+          levelOneContent.BID_PRICE !== bidData.price) &&
+        (levelOneContent.BID_SIZE &&
+          levelOneContent.BID_SIZE > 0 &&
+          levelOneContent.BID_SIZE !== bidData.size)
+      ) {
+        setBidData({ price: levelOneContent.BID_PRICE, size: levelOneContent.BID_SIZE })
+      }
     }
-  }, [levelOneContent])
+  }, [levelOneContent, askData, markData, bidData])
 
   return (
     <Card className="w-full h-full min-h-52">
-      {lastLevelOne === undefined ? (
+      {levelOneContent === undefined ? (
         <CardContent className="flex items-center justify-between">
           <p>Waiting for live data...</p>
         </CardContent>
@@ -32,36 +80,34 @@ export default function LevelOneCard({ levelOneContent }: LevelOneCardProps) {
           <div className="flex justify-between">
             <div className="flex flex-col">
               <span className="text-sm text-muted-foreground">Ask Price</span>
-              <span className="font-bold">{formatAmount(lastLevelOne.ASK_PRICE ?? 0, true)}</span>
+              <span className="font-bold">{formatAmount(askData.price ?? 0, true)}</span>
             </div>
             <div className="flex flex-col">
               <span className="text-sm text-muted-foreground">Ask Size</span>
-              <span className="font-bold">{lastLevelOne.ASK_SIZE ?? 'N/A'}</span>
+              <span className="font-bold">{askData.size ?? '-'}</span>
             </div>
           </div>
           <div className="flex justify-between items-center">
-            <span className="text-4xl font-bold">{formatAmount(lastLevelOne.MARK ?? 0, true)}</span>
+            <span className="text-4xl font-bold">{formatAmount(markData.mark ?? 0, true)}</span>
             <div className="flex flex-col items-end">
               <span className="text-primary font-bold">
-                {lastLevelOne.MARK_CHANGE && lastLevelOne.MARK_CHANGE > 0 ? '+' : ''}
-                {formatAmount(lastLevelOne.MARK_CHANGE ?? 0, true)}
+                {markData.change && markData.change > 0 ? '+' : '-'}
+                {formatAmount(markData.change ?? 0, true)}
               </span>
               <span className="text-muted-foreground text-sm">
-                {lastLevelOne.MARK_CHANGE_PERCENT && lastLevelOne.MARK_CHANGE_PERCENT > 0
-                  ? '+'
-                  : ''}
-                {formatPercentage((lastLevelOne.MARK_CHANGE_PERCENT ?? 0) / 100)}
+                {markData.percentChange && markData.percentChange > 0 ? '+' : '-'}
+                {formatPercentage((markData.percentChange ?? 0) / 100)}
               </span>
             </div>
           </div>
           <div className="flex justify-between">
             <div className="flex flex-col">
               <span className="text-sm text-muted-foreground">Bid Price</span>
-              <span className="font-bold">{lastLevelOne.BID_PRICE ?? 'N/A'}</span>
+              <span className="font-bold">{formatAmount(bidData.price ?? 0, true)}</span>
             </div>
             <div className="flex flex-col">
               <span className="text-sm text-muted-foreground">Bid Size</span>
-              <span className="font-bold">{lastLevelOne.BID_SIZE ?? 'N/A'}</span>
+              <span className="font-bold">{bidData.size ?? '-'}</span>
             </div>
           </div>
         </CardContent>
